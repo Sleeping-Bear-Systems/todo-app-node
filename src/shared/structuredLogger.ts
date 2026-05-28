2c949aa8de101e8de655dfa3b2e90b59d3633d4b
Charles Farris <charlesfarris71@gmail.com>
2026-05-28 17:01:35 -0600

    Fixed import extension

--- src/shared/structuredLogger.ts
+++ src/shared/structuredLogger.ts
@@ -6,7 +6,7 @@ import {
   type transport,
   transports,
 } from "winston";
-import type { AppConfig } from "./appConfig.ts";
+import type { AppConfig } from "./appConfig.js";
 
 export function createStructuredLogger(appConfig: AppConfig): Logger {
   // add console logging
