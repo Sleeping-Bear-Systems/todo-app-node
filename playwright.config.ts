7f5bfa75a542e1dd9fcdadfbccc352cb7a89b230
Charles Farris <charlesfarris71@gmail.com>
2026-05-31 01:21:59 -0600

    Use dev:e2e script for Playwright testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev:e2e",
     url: baseUrl,
     reuseExistingServer: !isCI,
   },
