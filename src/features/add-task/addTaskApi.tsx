02a7f10f3dc9059d0b06a6d5f4a5328e096b424d
Charles Farris <charlesfarris71@gmail.com>
2026-06-14 10:44:54 -0600

    Add schema for "Add Task" request
    - Update HTTP file

--- src/features/add-task/addTaskApi.tsx
+++ src/features/add-task/addTaskApi.tsx
@@ -1,8 +1,17 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import z from "zod";
 import type { AuthenticatedAppVariables } from "#shared/appVariables.js";
+
+const addTaskRequestSchema = z.object({
+  title: z.string(),
+  description: z.string().optional().default(""),
+});
+
+export type addTaskRequest = z.infer<typeof addTaskRequestSchema>;
 
 export const addTaskApi = new Hono<{
   Variables: AuthenticatedAppVariables;
-}>().post("/", (c) => {
+}>().post("/", zValidator("form", addTaskRequestSchema), (c) => {
   return c.json({}, 200);
 });
