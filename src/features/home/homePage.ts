b1cbca55ff57a1a6cdc006e476f7a09a71afd6af
Charles Farris <charlesfarris71@gmail.com>
2026-08-02 14:34:48 -0600

    Render tasks in a table on home page
    - Hide calendar

--- src/features/home/homePage.ts
+++ src/features/home/homePage.ts
@@ -1,21 +1,54 @@
+import type { PongoCollection } from "@event-driven-io/pongo";
 import { Hono } from "hono";
 import { html } from "hono/html";
+import {
+  type TaskDocument,
+  taskProjection,
+  tasksCollectionName,
+} from "#features/task-projection/taskProjection.ts";
 import type { AuthenticatedAppVariables } from "#shared/appVariables.ts";
 import { Page } from "#shared/page.ts";
 import { routes } from "#shared/routes.ts";
 
 export const homePage = new Hono<{
   Variables: AuthenticatedAppVariables;
-}>().get("/", (c) => {
+}>().get("/", async (c) => {
   const username = c.var.account.username;
   const headContent = html`
     <script src="/scripts/full-calendar/index.global.min.js" defer></script>
     <script src="/scripts/full-calendar/home-calendar.js" defer></script>
   `;
+  const readStore = c.var.readStore;
+
+  const tasks = await readStore
+    .db()
+    .collection<TaskDocument>(tasksCollectionName)
+    .find();
+
+  console.log(tasks);
+
   const content = html`
     <h1>Home</h1>
     <a class="button-link" href="${routes.ADD_TASK_PAGE}">Add Task</a>
-    <div id="home-calendar"></div>
+    <table>
+      <tr>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Status</th>
+      </tr>
+      ${tasks.map(
+        (d) => html`
+        <tr>
+          <td>${d.title}</td>
+          <td>${d.description}</td>
+          <td>${d.status}</td>
+        </tr>`,
+      )}
+    </table>
+
+    <div hidden>
+      <div id="home-calendar"></div>
+    <div>
   `;
 
   return c.html(
