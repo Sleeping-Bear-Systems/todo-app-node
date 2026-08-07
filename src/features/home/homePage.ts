48be6e2fe25a3f4ddf72459cad979453d6e51de1
Charles Farris <charlesfarris71@gmail.com>
2026-08-07 17:31:48 -0600

    Add cmplete and remove butons
    - Add endpoints
    - Add command handling

--- src/features/home/homePage.ts
+++ src/features/home/homePage.ts
@@ -1,7 +1,9 @@
+import { command, type HandleOptions } from "@event-driven-io/emmett";
 import { Hono } from "hono";
 import { html } from "hono/html";
 import type { AuthenticatedAppVariables } from "#shared/appVariables.ts";
-import { isDatastarRequest } from "#shared/datastar.ts";
+import { isDatastarRequest, sseRedirect } from "#shared/datastar.ts";
+import { handle, type TaskCommand } from "#shared/domain/taskCommand.ts";
 import {
   type TaskDocument,
   tasksCollectionName,
@@ -61,6 +63,7 @@ export const homePage = new Hono<{
             <th>Title</th>
             <th>Description</th>
             <th>Status</th>
+            <th>Action</th>
           </tr>
           ${tasks.map(
             (d) => html`
@@ -68,10 +71,90 @@ export const homePage = new Hono<{
               <td>${d.title}</td>
               <td>${d.description}</td>
               <td>${d.status}</td>
+              <td>
+                ${
+                  d.status === "Active"
+                    ? html`
+                      <form data-on:submit="@post('${routes.HOME_PAGE}/complete-task/${d._id}', {contentType: 'form'})">
+                        <button type="submit">Complete</button>
+                      </form>
+                      <form data-on:submit="@post('${routes.HOME_PAGE}/remove-task/${d._id}', {contentType: 'form'})">
+                        <button type="submit">Remove</button>
+                      </form>
+                    `
+                    : html``
+                }
+              </td>
             </tr>`,
           )}
         </table>
       </div>
       `;
     return c.html(content);
+  })
+  .post("/complete-task/:id", async (c) => {
+    // check Datastar request
+    if (!isDatastarRequest(c)) {
+      return c.json({ message: "Datastar request required" }, 400);
+    }
+
+    const taskId = c.req.param("id");
+    const userId = c.var.account.userId;
+    const requestId = c.var.requestId;
+    const now = c.var.clock.now();
+    const logger = c.var.logger;
+    const eventStore = c.var.eventStore;
+
+    const completeTaskCommand: TaskCommand = command<TaskCommand>(
+      "CompleteTask",
+      {
+        taskId,
+        completedOn: now,
+      },
+      {
+        now,
+        correlationId: requestId,
+        userId,
+      },
+    );
+    try {
+      await handle(eventStore, taskId, completeTaskCommand, {});
+      return sseRedirect(c, routes.HOME_PAGE);
+    } catch (error) {
+      logger.error(error);
+      return c.json({ message: "Internal server error" }, 500);
+    }
+  })
+  .post("/remove-task/:id", async (c) => {
+    // check Datastar request
+    if (!isDatastarRequest(c)) {
+      return c.json({ message: "Datastar request required" }, 400);
+    }
+
+    const taskId = c.req.param("id");
+    const userId = c.var.account.userId;
+    const requestId = c.var.requestId;
+    const now = c.var.clock.now();
+    const logger = c.var.logger;
+    const eventStore = c.var.eventStore;
+
+    const removeTaskCommand: TaskCommand = command<TaskCommand>(
+      "RemoveTask",
+      {
+        taskId,
+        removedOn: now,
+      },
+      {
+        now,
+        correlationId: requestId,
+        userId,
+      },
+    );
+    try {
+      await handle(eventStore, taskId, removeTaskCommand, {});
+      return sseRedirect(c, routes.HOME_PAGE);
+    } catch (error) {
+      logger.error(error);
+      return c.json({ message: "Internal server error" }, 500);
+    }
   });
